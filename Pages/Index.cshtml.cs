bddb2adc5a96decea230493f160c3678facbccf2
katia flores <katia.monterde15@outlook.com>
2022-10-02 15:56:16 -0500

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CFDIWEB.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    
+    }
+
+    public void OnPostGuardallave(String name, String lname){
+        Console.Write("Este es tu request");
+        Console.Write(lname);
+    }
+}
